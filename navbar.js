03075732528e65344ad4484eaf424e473e888ad0
KPanuwat-S <panuwatbood@gmail.com>
2023-03-24 15:22:43 +0700

    start project

---
+++ navbar.js
@@ -0,0 +1,11 @@
+const navBar = `<header>
+<div class="header-wrapper"><h1 class="web-logo"><span>Stang's</span> Portfolio</h1>
+<nav class="navbar">
+  <a href="./index.html">Home</a>
+  <a href="./education.html">Education</a>
+  <a href="./work.html">Work Experience</a>
+  <a class="btn" href="./contact.html">Contact Me</a>
+</nav></div>
+</header>`;
+
+document.body.insertAdjacentHTML("afterbegin", navBar);
